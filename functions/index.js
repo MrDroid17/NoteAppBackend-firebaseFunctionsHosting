10e82fdf8a78a35074b6c0ff3de84a11c33be645
Sobhit Kumar <sobhit2010@gmail.com>
2018-10-03 14:24:48 +0530

    testing functions

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/timestamp', (req, res) => {
-    res.send(`${Date.now}`)
+    res.send(`${Date.now()}`)
 })
-
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.app = functions.https.onRequest(app);
